790f22c79919151e02272e09f47bedb44c0dbe5c
Arun Sudhakar <arunsudhakar456@gmail.com>
2025-06-29 19:31:10 +0100

    Demo Commit

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-
+#demo test
 resource "aws_key_pair" "generated_key" {
   key_name   = "ca1-key"
   public_key = tls_private_key.ssh_key.public_key_openssh
